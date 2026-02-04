5ef5223339a633fc854c899490f5181ec4d7e642
Manuel <nuelyan2053@gmail.com>
2026-02-04 05:04:14 -0500

    Change greeting to use hardcoded name 'Alice'

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,8 @@ name=input("Alice")
 age=input(45)
 height=input(5.5)   # in feet
 
-print("Hello " + name)
+print("Hello " + Alice)
 print("You are " + str(45) + " years old")
 print("You are " + str(5.5) + " feet tall")
 print("Welcome to the program!")
+.
