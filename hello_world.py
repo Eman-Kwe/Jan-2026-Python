5c5741d9c9ea444e62782dba58e8d066cf2571e9
Eman-Kwe <nuelyan2053@gmail.com>
2026-02-04 04:40:47 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print ("Hello World!")
+print ("MY World!")
+
+name=input("Alice")
+age=input(45)
+height=input(5.5)   # in feet
+
+print("Hello " + name)
+print("You are " + str(45) + " years old")
+print("You are " + str(5.5) + " feet tall")
+print("Welcome to the program!")
